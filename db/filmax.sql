97a40b9cebaa9efe1f8ad48ae80cc29d557054ad
veshakov <143998753+veshakov@users.noreply.github.com>
2024-09-23 13:41:34 +0300

    uodate db file

--- db/filmax.sql
+++ db/filmax.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Сен 17 2024 г., 16:03
+-- Время создания: Сен 23 2024 г., 13:40
 -- Версия сервера: 8.0.19
 -- Версия PHP: 7.4.5
 
@@ -24,29 +24,76 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products`
+--
+
+CREATE TABLE `products` (
+  `id` int NOT NULL,
+  `img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `price` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `catigories` int NOT NULL DEFAULT '1',
+  `sales` int NOT NULL DEFAULT '0',
+  `description` text NOT NULL,
+  `pquntity` int NOT NULL DEFAULT '0',
+  `visibility` tinyint(1) NOT NULL DEFAULT '1',
+  `suppliy` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users`
 --
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
+  `role` tinyint(1) NOT NULL DEFAULT '1',
+  `family` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `patronymic` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `email` varchar(255) NOT NULL,
   `login` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `login`, `password`, `name`) VALUES
-(1, 'email@example.com', 'toster1', '12345', 'Тостер'),
-(2, 'email2@example.com', 'toster2', '123', 'Тостер 2'),
-(3, 'table01@examplo.lol', 'table0', '000', 'Стол');
+INSERT INTO `users` (`id`, `role`, `family`, `name`, `patronymic`, `email`, `login`, `password`) VALUES
+(1, 1, '', 'Тостер', '', 'email@example.com', 'toster1', '12345'),
+(2, 1, '', 'Тостер 2', '', 'email2@example.com', 'toster2', '123'),
+(3, 1, '', 'Стол', '', 'table01@examplo.lol', 'table0', '000');
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier` (`suppliy`),
+  ADD KEY `catigories` (`catigories`);
 
 --
 -- Индексы таблицы `users`
@@ -59,10 +106,32 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`catigories`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
