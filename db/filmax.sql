d1751d87e29df07a2ce4fb574bc3291776b00c5e
veshakov <143998753+veshakov@users.noreply.github.com>
2024-09-26 19:21:47 +0300

    Обновить архитектуру базы данных на паре 26.09.24

--- db/filmax.sql
+++ db/filmax.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Сен 23 2024 г., 13:40
+-- Время создания: Сен 26 2024 г., 14:26
 -- Версия сервера: 8.0.19
 -- Версия PHP: 7.4.5
 
@@ -35,6 +35,74 @@ CREATE TABLE `categories` (
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `cities`
+--
+
+CREATE TABLE `cities` (
+  `id` int NOT NULL,
+  `id_coutry` int NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `message`
+--
+
+CREATE TABLE `message` (
+  `id` int NOT NULL,
+  `id_user` int NOT NULL,
+  `id_product` int NOT NULL,
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='feedback - отзыв';
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `order-items`
+--
+
+CREATE TABLE `order-items` (
+  `id` int NOT NULL,
+  `id_user` int NOT NULL,
+  `id_product` int NOT NULL,
+  `id_order` int NOT NULL,
+  `quantity` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int NOT NULL,
+  `id_user` int NOT NULL,
+  `id_country` int NOT NULL,
+  `id_city` int NOT NULL,
+  `id_street` int NOT NULL,
+  `biulding` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `date-time` datetime NOT NULL,
+  `status` enum('В обработке','Собирается','Отправлен','Доставлен','Получен') NOT NULL,
+  `total-prise` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `products`
 --
 
@@ -42,13 +110,94 @@ CREATE TABLE `products` (
   `id` int NOT NULL,
   `img` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `price` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `price` double NOT NULL,
   `catigories` int NOT NULL DEFAULT '1',
   `sales` int NOT NULL DEFAULT '0',
   `description` text NOT NULL,
   `pquntity` int NOT NULL DEFAULT '0',
   `visibility` tinyint(1) NOT NULL DEFAULT '1',
   `suppliy` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `streets`
+--
+
+CREATE TABLE `streets` (
+  `id` int NOT NULL,
+  `id_sity` int NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `tel` varchar(20) NOT NULL,
+  `id_country` int NOT NULL,
+  `id_sity` int NOT NULL,
+  `id_street` int NOT NULL,
+  `bilding` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `supplies`
+--
+
+CREATE TABLE `supplies` (
+  `id` int NOT NULL,
+  `id_suplier` int NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `supply_content`
+--
+
+CREATE TABLE `supply_content` (
+  `id_supply` int NOT NULL,
+  `id_product` int NOT NULL,
+  `quantity` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `support-answer`
+--
+
+CREATE TABLE `support-answer` (
+  `id` int NOT NULL,
+  `id_request` int NOT NULL,
+  `id_admin` int NOT NULL,
+  `ansfer` text NOT NULL,
+  `date-time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `support-request`
+--
+
+CREATE TABLE `support-request` (
+  `id` int NOT NULL,
+  `id_user` int NOT NULL,
+  `message` text NOT NULL,
+  `date-time` datetime NOT NULL,
+  `status` enum('Новый','Закрыт','Отклонён','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -88,12 +237,97 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_coutry` (`id_coutry`);
+
+--
+-- Индексы таблицы `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_product` (`id_product`);
+
+--
+-- Индексы таблицы `order-items`
+--
+ALTER TABLE `order-items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`,`id_product`,`id_order`),
+  ADD KEY `id_product` (`id_product`),
+  ADD KEY `id_order` (`id_order`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`,`id_country`,`id_city`,`id_street`),
+  ADD KEY `id_street` (`id_street`),
+  ADD KEY `id_country` (`id_country`),
+  ADD KEY `id_city` (`id_city`);
+
+--
 -- Индексы таблицы `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
   ADD KEY `supplier` (`suppliy`),
   ADD KEY `catigories` (`catigories`);
+
+--
+-- Индексы таблицы `streets`
+--
+ALTER TABLE `streets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_sity` (`id_sity`);
+
+--
+-- Индексы таблицы `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_country` (`id_country`),
+  ADD KEY `id_sity` (`id_sity`),
+  ADD KEY `id_street` (`id_street`);
+
+--
+-- Индексы таблицы `supplies`
+--
+ALTER TABLE `supplies`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_suplier` (`id_suplier`);
+
+--
+-- Индексы таблицы `supply_content`
+--
+ALTER TABLE `supply_content`
+  ADD KEY `id_supply` (`id_supply`,`id_product`),
+  ADD KEY `id_product` (`id_product`);
+
+--
+-- Индексы таблицы `support-answer`
+--
+ALTER TABLE `support-answer`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_request` (`id_request`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Индексы таблицы `support-request`
+--
+ALTER TABLE `support-request`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Индексы таблицы `users`
@@ -112,9 +346,69 @@ ALTER TABLE `categories`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT для таблицы `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `order-items`
+--
+ALTER TABLE `order-items`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT для таблицы `products`
 --
 ALTER TABLE `products`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `streets`
+--
+ALTER TABLE `streets`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `supplies`
+--
+ALTER TABLE `supplies`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `support-answer`
+--
+ALTER TABLE `support-answer`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `support-request`
+--
+ALTER TABLE `support-request`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
@@ -128,10 +422,75 @@ ALTER TABLE `users`
 --
 
 --
+-- Ограничения внешнего ключа таблицы `cities`
+--
+ALTER TABLE `cities`
+  ADD CONSTRAINT `cities_ibfk_1` FOREIGN KEY (`id_coutry`) REFERENCES `countries` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `message`
+--
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `order-items`
+--
+ALTER TABLE `order-items`
+  ADD CONSTRAINT `order-items_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order-items_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order-items_ibfk_3` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`id_street`) REFERENCES `streets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`id_country`) REFERENCES `countries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`id_city`) REFERENCES `cities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orders_ibfk_4` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Ограничения внешнего ключа таблицы `products`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`catigories`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `streets`
+--
+ALTER TABLE `streets`
+  ADD CONSTRAINT `streets_ibfk_1` FOREIGN KEY (`id_sity`) REFERENCES `cities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD CONSTRAINT `suppliers_ibfk_1` FOREIGN KEY (`id_sity`) REFERENCES `cities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `suppliers_ibfk_2` FOREIGN KEY (`id_country`) REFERENCES `countries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `suppliers_ibfk_3` FOREIGN KEY (`id_street`) REFERENCES `streets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `suppliers_ibfk_4` FOREIGN KEY (`id`) REFERENCES `supplies` (`id_suplier`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `supply_content`
+--
+ALTER TABLE `supply_content`
+  ADD CONSTRAINT `supply_content_ibfk_1` FOREIGN KEY (`id_supply`) REFERENCES `supplies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `supply_content_ibfk_2` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `support-answer`
+--
+ALTER TABLE `support-answer`
+  ADD CONSTRAINT `support-answer_ibfk_1` FOREIGN KEY (`id_request`) REFERENCES `support-request` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `support-answer_ibfk_2` FOREIGN KEY (`id_admin`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `support-request`
+--
+ALTER TABLE `support-request`
+  ADD CONSTRAINT `support-request_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
